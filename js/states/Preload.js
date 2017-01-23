b828ae993b282c187f68a5cf6a4160d14848fd2f
Josh Naylor <joshnaylor88@gmail.com>
2017-01-23 15:10:00 -0600

    fixed audio path

--- js/states/Preload.js
+++ js/states/Preload.js
@@ -18,7 +18,7 @@ MrHop.PreloadState = {
     this.load.image('background', 'assets/images/background.png');
     this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 51, 67, 5, 2, 3);
     this.load.audio('coin', ['assets/audio/coin.mp3', 'assets/audio/coin.ogg']);
-    this.load.audio('jungle_map', ['assets/audio/jungle-map-jack-tell.mp3', 'jungle-map-jack-tell.ogg']);
+    this.load.audio('jungle_map', ['assets/audio/jungle-map-jack-tell.mp3', 'assets/audio/jungle-map-jack-tell.ogg']);
   },
   create: function() {
     var audio = this.game.add.audio('jungle_map')
